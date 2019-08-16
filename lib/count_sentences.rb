cc6075a9dd7776040e3f90008c52f3cf7b597ba8
Renee Sims <info@reneesoul.com>
2019-08-16 02:12:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,14 +20,14 @@ class String
     sentences = []
     
     
-    spliters.each do |spliter|
-      if self.end_with?(".", "?", "!", "!!", "...")
-        self.squeeze(spliter)
+    # spliters.each do |spliter|
+    #   if self.end_with?(".", "?", "!", "!!", "...")
+    #     self.squeeze(spliter)
         sentences << self.split(/\b[.|?|!]/)
         # sentences << self.split("?")
         # sentences << self.split("!")
-      end
-    end
+    #   end
+    # end
     sentences << self.scan(/\b[.|?|!]/)
 # binding.pry   
     sentences 
