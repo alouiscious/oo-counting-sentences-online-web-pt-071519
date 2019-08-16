35181a8c3f40e0b8f41c5038c48e6b69b968dac3
Renee Sims <info@reneesoul.com>
2019-08-16 02:04:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    spliters = [".", "?", "!"]
+    sentences = []
+    
+    
+    spliters.each do |spliter|
+      if self.end_with?(".", "?", "!", "!!", "...")
+        self.squeeze(spliter)
+        sentences << self.split(spliter)
+        # sentences << self.split("?")
+        # sentences << self.split("!")
+      end
+    end
+# binding.pry   
+    sentences 
+    sentences.count
+    
+    
   end
+
 end
